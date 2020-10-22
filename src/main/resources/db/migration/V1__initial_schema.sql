4406c76e4b65c63e219836f2123d8d9dfc12f138
Michał Prawda <m.prawda99@gmail.com>
2020-10-22 21:46:00 +0200

    Add basic database (#1)
    * Add basic database
    
    * Add changes requested in pull request #1
    
    * Fix id in schema

---
+++ src/main/resources/db/migration/V1__initial_schema.sql
@@ -0,0 +1,28 @@
+CREATE TABLE users (
+    id BIGINT GENERATED ALWAYS AS IDENTITY,
+    name TEXT NOT NULL,
+    last_name TEXT,
+    date_of_birth DATE,
+    created_on TIMESTAMPZ NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE posts (
+    id BIGINT GENERATED ALWAYS AS IDENTITY,
+    contents TEXT NOT NULL,
+    user_id BIGINT NOT NULL,
+    date_added TIMESTAMPZ NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (user_id) REFERENCES users (id)
+);
+
+CREATE TABLE comments (
+    id BIGINT GENERATED ALWAYS AS IDENTITY,
+    contents TEXT NOT NULL,
+    post_id BIGINT NOT NULL,
+    user_id BIGINT NOT NULL,
+    date_added TIMESTAMPZ NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (post_id) REFERENCES posts (id),
+    FOREIGN KEY (user_id) REFERENCES users (id)
+);
