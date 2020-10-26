d772a819eadffb41f9c9979d4c53069d3ca37725
Michał Prawda <m.prawda99@gmail.com>
2020-10-26 20:37:36 +0100

    Add CRUD for posts, add ".idea/" to .gitignore

--- src/main/scala/io/rubduk/domain/package.scala
+++ src/main/scala/io/rubduk/domain/package.scala
@@ -1,8 +1,10 @@
 package io.rubduk
 
 import io.rubduk.domain.repositories.UserRepository
+import io.rubduk.domain.repositories.PostRepository
 import zio.Has
 
 package object domain {
   type UserRepository = Has[UserRepository.Service]
+  type PostRepository = Has[PostRepository.Service]
 }
