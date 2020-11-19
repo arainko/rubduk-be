1416b9374ddeb7ed259c5fa2e631304456369d9d
Aleksander Rainko <aleksander.rainko99@gmail.com>
2020-11-19 10:09:45 +0100

    generic shenanigans

--- src/main/scala/io/rubduk/domain/filters.scala
+++ src/main/scala/io/rubduk/domain/filters.scala
@@ -1,10 +1,21 @@
 package io.rubduk.domain
 
+import cats.implicits.catsSyntaxOptionId
 import io.rubduk.infrastructure.additional.Filter
 import io.rubduk.infrastructure.additional.ImprovedPostgresProfile.api._
 import io.rubduk.infrastructure.models.UserId
 import io.rubduk.infrastructure.tables.{Comments, Posts}
 import io.rubduk.infrastructure.typeclasses.IdConverter._
+
+import shapeless._
+import ops.tuple.FlatMapper
+import syntax.std.tuple._
+import test._
+
+object mapper extends Poly1 {
+  implicit def atSingle[A] = at[Tuple1[A]](identity)
+  implicit def atTuple[A, B] = at[(A, B)](a => Tuple2(a_.1, a._2))
+}
 
 object filters {
 
@@ -13,4 +24,17 @@ object filters {
 
   def commentUserIdFilter(userId: Option[UserId]): Filter[Comments.Schema] =
     Filter.optional(userId)(_.userId === _)
+
+  def costam: Filter[((Comments.Schema, Posts.Schema), Posts.Schema)] =
+
+      commentUserIdFilter(UserId(1).some)
+      .combine(commentUserIdFilter(UserId(1).some))
+      .combine(commentUserIdFilter(UserId(1).some))
+        .combine(postUserIdFilter(UserId(1).some))
+        .combine(postUserIdFilter(UserId(1).some))
+
+  def de: Filter[(Comments.Schema, Posts.Schema, Posts.Schema)] = Filter.flatten(costam) { a =>
+    a.productElements.collect()
+  }
+
 }
--- src/main/scala/io/rubduk/infrastructure/additional/Filter.scala
+++ src/main/scala/io/rubduk/infrastructure/additional/Filter.scala
@@ -1,14 +1,44 @@
 package io.rubduk.infrastructure.additional
 
+import io.rubduk.infrastructure.additional.ImprovedPostgresProfile.api._
 import slick.lifted.{Query, Rep}
-import ImprovedPostgresProfile.api._
 
-trait Filter[T] {
+import shapeless._
+import ops.tuple.FlatMapper
+import syntax.std.tuple._
+import test._
+import cats.implicits._
+
+trait LowPriorityFlatten extends Poly1 {
+  implicit def default[T] = at[T](Tuple1(_))
+}
+object tFlatten extends LowPriorityFlatten {
+  implicit def caseTuple[P <: Product](implicit lfm: Lazy[FlatMapper[P, tFlatten.type]]) =
+    at[P](lfm.value(_))
+}
+
+trait Filter[A] { self =>
   def isApplicable: Boolean
-  def apply(table: T): Rep[Boolean]
+  def apply(table: A): Rep[Boolean]
+
+  final def combine[B](f2: Filter[B]): Filter[(A, B)] = new Filter[(A, B)] {
+    override def isApplicable: Boolean = self.isApplicable && f2.isApplicable
+    override def apply(table: (A, B)): Rep[Boolean] = self.apply(table._1) && f2.apply(table._2)
+  }
+
+  final def combine(f2: Filter[A]): Filter[A] = new Filter[A] {
+    override def isApplicable: Boolean = self.isApplicable && f2.isApplicable
+    override def apply(table: A): Rep[Boolean] = self.apply(table) && f2.apply(table)
+  }
 }
 
 object Filter {
+
+  def flatten[A <: Product, B <: Product](f: Filter[A])(func: B => A): Filter[B] = new Filter[B] {
+    override def isApplicable: Boolean = f.isApplicable
+
+    override def apply(table: B): Rep[Boolean] = f.apply(func(table))
+  }
 
   def optional[T, A](applier: Option[A])(predicate: (T, A) => Rep[Boolean]): Filter[T] =
     OptionFilter(applier, applier.map(a => predicate(_: T, a)).getOrElse(_ => true))
