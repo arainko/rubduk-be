41e019250c24b4d068f7d698b5d1b7ee113c8e96
Aleksander Rainko <aleksander.rainko99@gmail.com>
2020-12-03 22:07:47 +0100

    added more information in comments (#21)

--- src/main/scala/io/rubduk/domain/services/CommentService.scala
+++ src/main/scala/io/rubduk/domain/services/CommentService.scala
@@ -2,15 +2,14 @@ package io.rubduk.domain.services
 
 import java.time.OffsetDateTime
 
-import cats.syntax.functor._
-import io.rubduk.domain.{CommentRepository, PostRepository}
 import io.rubduk.domain.errors.ApplicationError
 import io.rubduk.domain.errors.ApplicationError.ServerError
 import io.rubduk.domain.errors.CommentError._
 import io.rubduk.domain.errors.PostError.PostNotFound
+import io.rubduk.domain.errors.UserError.UserNotFound
 import io.rubduk.domain.repositories.{CommentRepository, PostRepository}
+import io.rubduk.domain.{CommentRepository, PostRepository, UserRepository}
 import io.rubduk.infrastructure.additional.Filter
-import io.rubduk.infrastructure.models.Page._
 import io.rubduk.infrastructure.models._
 import io.rubduk.infrastructure.tables.Comments
 import zio.ZIO
@@ -22,40 +21,41 @@ object CommentService {
     offset: Offset,
     limit: Limit,
     filters: Filter[Comments.Schema]*
-  ): ZIO[CommentRepository with PostRepository, ApplicationError, Page[Comment]] =
-    PostRepository
-      .getById(postId)
-      .someOrFail(PostNotFound)
-      .zipRight {
-        CommentRepository
-          .getByPostIdPaginated(postId, offset, limit, filters)
-          .map(_.map(_.toDomain))
+  ): ZIO[CommentRepository with PostRepository with UserRepository, ApplicationError, Page[Comment]] =
+    for {
+      _ <- PostRepository.getById(postId).someOrFail(PostNotFound)
+      comments <- CommentRepository.getByPostIdPaginated(postId, offset, limit, filters)
+      mergedComments <- ZIO.foreachPar(comments.entities) { comment =>
+        UserService.getById(comment.userId).map(comment.toDomain)
       }
+    } yield Page(mergedComments, comments.count)
 
   def getByPostId(
     postId: PostId,
     offset: Offset,
     limit: Limit,
     filters: Filter[Comments.Schema]*
-  ): ZIO[CommentRepository with PostRepository, ApplicationError, Seq[Comment]] =
-    PostRepository
-      .getById(postId)
-      .someOrFail(PostNotFound)
-      .zipRight {
-        CommentRepository
-          .getByPostId(postId, offset, limit, filters)
-          .map(_.map(_.toDomain))
+  ): ZIO[CommentRepository with PostRepository with UserRepository, ApplicationError, Seq[Comment]] =
+    for {
+      _ <- PostRepository.getById(postId).someOrFail(PostNotFound)
+      comments <- CommentRepository.getByPostId(postId, offset, limit, filters)
+      mergedComments <- ZIO.foreachPar(comments) { comment =>
+        UserService.getById(comment.userId).map(comment.toDomain)
       }
+    } yield mergedComments
 
-  def getById(postId: PostId, commentId: CommentId): ZIO[CommentRepository, ApplicationError, Comment] =
+
+  def getById(postId: PostId, commentId: CommentId): ZIO[CommentRepository with UserRepository, ApplicationError, Comment] =
     CommentRepository
       .getById(commentId)
       .someOrFail(CommentNotFound)
       .filterOrFail(_.postId == postId)(CommentNotUnderPost)
-      .map(_.toDomain)
+      .flatMap { comment =>
+        UserService.getById(comment.userId).map(comment.toDomain)
+      }
 
   def insert(postId: PostId, userId: UserId, comment: CommentDTO): ZIO[CommentRepository, ServerError, CommentId] = {
-    val commentToInsert = comment.toDomain(postId, userId, OffsetDateTime.now()).toDAO
+    val commentToInsert = comment.toDAO(postId, userId, OffsetDateTime.now())
     CommentRepository.insert(postId, commentToInsert)
   }
 
@@ -64,12 +64,13 @@ object CommentService {
     postId: PostId,
     commentId: CommentId,
     comment: CommentDTO
-  ): ZIO[CommentRepository, ApplicationError, Unit] =
-    CommentRepository
-      .getById(commentId)
-      .someOrFail(CommentNotFound)
-      .filterOrFail(_.userId == userId)(CommentNotByThisUser)
-      .filterOrFail(_.postId == postId)(CommentNotUnderPost)
-      .zipRight(CommentRepository.update(commentId, comment.contents))
-      .unit
+  ): ZIO[CommentRepository with UserRepository, ApplicationError, Unit] =
+    for {
+      fetchedComment <- getById(postId, commentId)
+      _ <- ZIO.succeed(fetchedComment.user.id)
+        .someOrFail(UserNotFound)
+        .unrefineTo[ApplicationError]
+        .filterOrFail(_ == userId)(CommentNotByThisUser)
+      _ <- CommentRepository.update(commentId, comment.contents)
+    } yield ()
 }
--- src/main/scala/io/rubduk/infrastructure/models/comment.scala
+++ src/main/scala/io/rubduk/infrastructure/models/comment.scala
@@ -2,6 +2,7 @@ package io.rubduk.infrastructure.models
 
 import java.time.OffsetDateTime
 
+import cats.implicits.catsSyntaxOptionId
 import io.scalaland.chimney.dsl._
 
 final case class CommentId(value: Long) extends AnyVal
@@ -13,31 +14,53 @@ final case class CommentDAO(
   userId: UserId,
   dateAdded: OffsetDateTime
 ) {
-  def toDomain: Comment = this.transformInto[Comment]
-  def toDTO: CommentDTO = this.transformInto[CommentDTO]
+  def toDomain(user: User): Comment = this
+    .into[Comment]
+    .withFieldConst(_.user, user)
+    .transform
+
 }
 
 case class Comment(
   id: Option[CommentId],
   contents: String,
   postId: PostId,
-  userId: UserId,
+  user: User,
   dateAdded: OffsetDateTime
 ) {
-  def toDAO: CommentDAO = this.transformInto[CommentDAO]
-  def toDTO: CommentDTO = this.transformInto[CommentDTO]
+  def toDAO(userId: UserId): CommentDAO =
+    this.into[CommentDAO]
+      .withFieldConst(_.userId, userId)
+      .transform
+
+  def toDTO: CommentDTO =
+    this
+      .into[CommentDTO]
+      .withFieldComputed(_.name, _.user.name.some)
+      .withFieldComputed(_.lastName, _.user.lastName)
+      .transform
 }
 
 case class CommentDTO(
   id: Option[CommentId],
-  contents: String
+  contents: String,
+  name: Option[String],
+  lastName: Option[String],
+  dateAdded: Option[OffsetDateTime]
 ) {
 
-  def toDomain(postId: PostId, userId: UserId, dateAdded: OffsetDateTime): Comment =
+  def toDAO(postId: PostId, userId: UserId, dateAdded: OffsetDateTime): CommentDAO =
+    this.into[CommentDAO]
+      .withFieldConst(_.postId, postId)
+      .withFieldConst(_.userId, userId)
+      .withFieldConst(_.dateAdded, dateAdded)
+      .transform
+
+  def toDomain(postId: PostId, user: User, dateAdded: OffsetDateTime): Comment =
     this
       .into[Comment]
       .withFieldConst(_.postId, postId)
-      .withFieldConst(_.userId, userId)
+      .withFieldConst(_.user, user)
       .withFieldConst(_.dateAdded, dateAdded)
       .transform
 }
